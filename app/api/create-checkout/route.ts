ceac1a64f65fab52233c1cf1de879b7863d6775b
truckingwise <info@dispatchtrucks.com>
2025-12-16 19:19:48 -0500

    Fix Stripe API version compatibility

--- app/api/create-checkout/route.ts
+++ app/api/create-checkout/route.ts
@@ -3,9 +3,7 @@ import Stripe from 'stripe'
 
 export const runtime = 'nodejs'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function POST() {
   try {
