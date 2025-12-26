3d354757c38fb45e10f984dd62d3e92f04bed884
truckingwise <info@dispatchtrucks.com>
2025-12-25 19:11:55 -0500

    Add Meta Pixel tracking (ID: 1571587700797342)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'HaulVerify V1 - Check Brokers Before You Haul',
@@ -12,7 +13,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <head>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1571587700797342');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+      <body className="antialiased">
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1571587700797342&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   )
 }
